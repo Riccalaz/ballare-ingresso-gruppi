019afe918447e039c98458b5f355a77806ddfe76
Riccardo Lazzari <153769506+Riccalaz@users.noreply.github.com>
2024-10-01 20:13:51 +0200

    Update thread.java

--- thread.java
+++ thread.java
@@ -4,31 +4,31 @@ import java.util.Random;
 
 public class thread extends Thread{
 	private cp contatore;
-	private int npersone;
+	private int npersone;  //numero di persone per gruppo
 	
 	public thread(cp c){
 		contatore=c;
 		
 		Random ran = new Random();
 		
-		npersone=ran.nextInt(10)+1;
+		npersone=ran.nextInt(10)+1; //genero casulamento il numero di persone di un gruppi
 	}
 	
 	public void run() {
 		
 		Random ran = new Random();
-		System.out.println("persone nel gruppo: "+npersone);
+		System.out.println("persone nel gruppo: "+npersone); //stampo a schermo la prima volta il numero di persone presenti in un gruppi
 		
 		while(true) {
 			contatore.entra(npersone);
 			try {
-				Thread.sleep(ran.nextInt(1000)+100);
+				Thread.sleep(ran.nextInt(1000)+100);  //attendo un numero casuale di millisecondi
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 			contatore.esce(npersone);
 			try {
-				Thread.sleep(ran.nextInt(1000)+100);
+				Thread.sleep(ran.nextInt(1000)+100); //attendo un numero casuale di millisecondi
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
